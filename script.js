4559c29bbef3a0720c5fb9357700c10056abe255
Aline DUBOIS <juillet.aline@gmail.com>
2020-08-31 21:16:16 +0200

    mise en forme tableau

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-const message = “Merci d'avoir envoyé votre message à la Famille Royale.Nous vous répondrons dans les meilleurs délais”
+const message ="Merci pour votre message à la Famille Royale.Nous vous répondrons dans les meilleurs délais”
 
 document.getElementById("contactForm").addEventListener('submit', function(event) {
     event.preventDefault();
     alert(message);
+
