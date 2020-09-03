81343a93b2e613ed46cfcbfc0284dcc23e78ec75
Aline DUBOIS <juillet.aline@gmail.com>
2020-09-03 21:29:50 +0200

    finalisation projet technique !

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-const message ="Merci pour votre message à la Famille Royale.Nous vous répondrons dans les meilleurs délais”
+const message ="Merci pour votre message à la Famille Royale.Nous vous répondrons dans les meilleurs délais";
 
-document.getElementById("contactForm").addEventListener('submit', function(event) {
+document.getElementById("formulaire").addEventListener('submit', function(event) {
     event.preventDefault();
     alert(message);
+});
 
