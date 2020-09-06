cf3cf7698a4d463e76af6a82dec8a7d13c946b5d
Aline DUBOIS <juillet.aline@gmail.com>
2020-09-06 14:50:34 +0200

    projet fini

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const message ="Merci pour votre message à la Famille Royale.Nous vous répondrons dans les meilleurs délais";
 
-document.getElementById("formulaire").addEventListener('submit', function(event) {
+document.getElementById("formulaire").addEventListener('submit', function (event) {
     event.preventDefault();
     alert(message);
 });
